8f249fc68facdb41e7d1597567ae027f7bed0da7
ingrismartinez <ingris_mari@hotmail.com>
2020-01-10 04:52:55 -0600

    Adding Initial Project

---
+++ Model/Budget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETDevChallenge.Model
+{
+    public class Budget
+    {
+        public int UId { get; set; }
+    }
+}
